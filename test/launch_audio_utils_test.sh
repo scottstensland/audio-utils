ca7d4db3d7e202d3a3df0ab93c7410a2f3fd84ee
Scott Stensland <scottstensland@gmail.com>
2014-07-25 12:29:10 -0400

    new file:   launch_audio_utils_test.sh

---
+++ test/launch_audio_utils_test.sh
@@ -0,0 +1,6 @@
+
+# node ./test_audio_utils.js  dev   # use this - pulls in other libs from local install
+
+node ./test_audio_utils.js  nubia # this flavor is for module owner ONLY - pulls in tinkering versions
+
+
