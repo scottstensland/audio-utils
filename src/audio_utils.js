400b07f20525e037614a2b4aa44356bd0b0158e8
Scott Stensland <scottstensland@gmail.com>
2020-08-03 08:48:42 -0400

    modified:   src/audio_utils.js 	modified:   test/test_audio_utils.js
    	updated audio buffer synthesis to allow input of chosen frequency

--- src/audio_utils.js
+++ src/audio_utils.js
@@ -474,6 +474,8 @@ var pop_audio_buffer = function (size_buff, given_samples_per_cycle) {
 
             audio_obj.buffer[running_index] = Math.sin(theta);
 
+            console.log(audio_obj.buffer[running_index] , "theta ", theta);
+
             theta += incr_theta;
             running_index++;
 
@@ -485,4 +487,91 @@ var pop_audio_buffer = function (size_buff, given_samples_per_cycle) {
 };       //      pop_audio_buffer
 
 exports.pop_audio_buffer = pop_audio_buffer;
+
+
+// var pop_audio_buffer_custom = function (size_buff, given_samples_per_cycle) {
+var pop_audio_buffer_custom = function (number_of_samples, given_freq, samples_per_second) {
+
+// func Pop_audio_buffer(number_of_samples float64, given_freq float64, samples_per_second float64) ([]float64, error) {
+
+    console.log("in pop_audio_buffer_custom "); 
+
+    /*
+
+    output sinusoidal curve is assured to both start and stop at the zero cross over threshold,
+    independent of supplied input parms which control samples per cycle and buffer size.
+    This avoids that "pop" which otherwise happens when rendering audio curves
+    which begins at say 0.5 of a possible range -1 to 0 to +1
+
+    */
+
+    // give a default value if not supplied by input parm
+
+    // var samples_per_cycle = (typeof given_samples_per_cycle != "undefined") ? 
+    //                                 given_samples_per_cycle : 64;
+
+
+    // var samples_per_cycle = given_samples_per_cycle || 64;
+
+    var number_of_samples = Math.round(number_of_samples);
+
+    console.log("number_of_samples ", number_of_samples);
+    console.log("given_freq ", given_freq);
+    console.log("samples_per_second ", samples_per_second);
+
+    // if (samples_per_cycle > size_buff) {
+
+    //     console.log("ERROR - SIZE_BUFFER_SOURCE MUST be larger than samples_per_cycle");
+    //     process.exit(1);
+    // }
+
+    /*
+
+    // stens TODO - verify if we leave this commented OUT the rendered sound has NO audible POP
+    
+    if (0 != size_buff % samples_per_cycle) {
+
+        console.log("ERROR - samples_per_cycle MUST be a divisor to SIZE_BUFFER_SOURCE");
+        process.exit(2);
+    }
+    */
+
+    // var count_num_cycles = size_buff / samples_per_cycle;
+
+    // console.log("in pop source buffer  size_buff ", size_buff, 
+    //             " samples_per_cycle ",samples_per_cycle);
+
+    var audio_obj = {};
+    
+    var source_buffer = new Float32Array(number_of_samples);
+
+    audio_obj.buffer = source_buffer;
+    var running_index = 0;
+
+    // var num_samples_per_cycle = size_buff / count_num_cycles;
+
+    // console.log("count_num_cycles ", count_num_cycles, 
+    //             " num_samples_per_cycle ", audio_obj.num_samples_per_cycle);
+
+    // var incr_theta = (2.0 * Math.PI) / num_samples_per_cycle;
+    var incr_theta = (2.0 * Math.PI * given_freq) / samples_per_second;
+    var theta = 0.0;
+
+    console.log("incr_theta ", incr_theta);
+
+    // for (var index = 0; index < count_num_cycles; index++) {
+    for (var curr_sample = 0; curr_sample < number_of_samples; curr_sample++) {
+
+        audio_obj.buffer[curr_sample] = Math.sin(theta);
+
+        console.log(audio_obj.buffer[curr_sample] , "theta ", theta);
+
+        theta += incr_theta;
+    }
+    
+    return audio_obj;
+
+};       //      pop_audio_buffer_custom
+
+exports.pop_audio_buffer_custom = pop_audio_buffer_custom;
 
--- test/test_audio_utils.js
+++ test/test_audio_utils.js
@@ -92,7 +92,7 @@ var synth_curve_write_to_wav_file_then_read_back = function () {
 
     // shared_utils.write_32_bit_float_buffer_to_16_bit_wav_file(source_obj, source_wave_filename);
 
-	shared_utils.write_32_bit_float_buffer_to_16_bit_wav_file(source_obj, source_wave_filename);
+    shared_utils.write_32_bit_float_buffer_to_16_bit_wav_file(source_obj, source_wave_filename);
 
 
 // var read_wav_file = function(input_filename, cb_read_file_done) {
@@ -128,13 +128,91 @@ var synth_curve_write_to_wav_file_then_read_back = function () {
                                     cb_read_file_done);
 */
 
-	shared_utils.read_wav_file(wav_file_input_obj.filename, cb_read_file_done);
+    shared_utils.read_wav_file(wav_file_input_obj.filename, cb_read_file_done);
 
 
 
 };      //      synth_curve_write_to_wav_file_then_read_back
 
-synth_curve_write_to_wav_file_then_read_back();
+var synth_curve_write_to_wav_file_then_read_back_custom = function () {
+
+    // ---------- generates sin tone ------------- //
+
+    var local_output_dir = process.env.AUDIO_DIR || process.env.HOME;
+
+    var output_dir = resolvePath(local_output_dir);
+
+    var output_format = ".wav";
+
+    console.log(" output_dir ", output_dir);
+
+    // ----------------
+
+    // var SIZE_BUFFER_SOURCE = 256;
+    SIZE_BUFFER_SOURCE = 16384;   //   number of audio samples to synthesize ( points on the raw audio curve )
+
+    var samples_per_second = 44100;  //  CD quality audio
+    var given_freq = 300;   //   synthesize audio at this frequency
+
+    var source_obj = {};
+
+    var source_obj = audio_utils.pop_audio_buffer_custom(SIZE_BUFFER_SOURCE, given_freq, samples_per_second);
+
+    // var max_index = 3;
+    var max_index = 8; // only show this number of entries
+    // var max_index = SIZE_BUFFER_SOURCE;
+
+    for (var index = 0; index < max_index; index++) {
+
+        console.log(index, " pop_audio_buffer_custom ", source_obj.buffer[index]);
+    }
+
+    // ---------- write to output file ------------- //
+
+    var source_wave = "audio_util_test_file_custom";
+
+    var source_wave_filename = path.join(output_dir, source_wave + output_format);
+
+    console.log("source_wave_filename   ", source_wave_filename);
+
+    shared_utils.write_32_bit_float_buffer_to_16_bit_wav_file(source_obj, source_wave_filename);
+
+    console.log("source_wave_filename   ", source_wave_filename);
+
+    // ------------ read wav file -------------------- //
+
+    console.log("\n\nread wav file\n\n");
+
+    var wav_file_input_obj = {};  // create stub object to which we attach .buffer
+
+    var property_buffer_raw_input_file = "buffer_raw_input_file";
+    var property_buffer_input_file     = "buffer_input_file";
+
+    wav_file_input_obj.filename = source_wave_filename;
+
+    // wav_file_input_obj[property_buffer_raw_input_file] = new Buffer(0); // gives (node:429147) [DEP0005] DeprecationWarning: Buffer() is deprecated due to security and usability issues. Please use the Buffer.alloc(), Buffer.allocUnsafe(), or Buffer.from() methods instead.
+
+    wav_file_input_obj[property_buffer_raw_input_file] = new Buffer.alloc(0);
+
+    console.log("abouttttt to read wav_file_input_obj.filename ", wav_file_input_obj.filename);
+
+    var spec = {};
+
+/*
+    shared_utils.read_16_bit_wav_file_into_32_bit_float_buffer(
+                                    wav_file_input_obj,
+                                    wav_file_input_obj.filename, 
+                                    spec,
+                                    cb_read_file_done);
+*/
+
+    shared_utils.read_wav_file(wav_file_input_obj.filename, cb_read_file_done);
+
+};      //      synth_curve_write_to_wav_file_then_read_back_custom
+
+// synth_curve_write_to_wav_file_then_read_back();
+
+synth_curve_write_to_wav_file_then_read_back_custom();
 
 
 console.log("<><><>  <><><>  <><><>   end of processing   <><><>  <><><>  <><><>");
